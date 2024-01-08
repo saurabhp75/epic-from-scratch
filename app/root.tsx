c24ff0524bf8f447ffc79d3c00c0a071e039a144
Saurabh Prakash <saurabhp75@gmail.com>
2024-01-08 18:48:42 +0530

    Sync with workshop and add routes

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,12 @@
+import os from 'node:os'
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { type LinksFunction, json } from "@remix-run/node";
 import {
   Links,
   LiveReload,
-  Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
 import faviconAssetUrl from './assets/favicon.svg'
@@ -32,21 +32,42 @@ export const links: LinksFunction = () => {
   ]
 };
 
+export async function loader() {
+	return json({ username: os.userInfo().username })
+}
+
 export default function App() {
+	const data = useLoaderData<typeof loader>()
   return (
-    <html lang="en">
+    <html lang="en" className="h-full overflow-x-hidden">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+      <body className="flex h-full flex-col justify-between bg-background text-foreground">
+				<header className="container mx-auto py-6">
+					<nav className="flex justify-between">
+						<div>
+							<div className="font-light">epic</div>
+							<div className="font-bold">notes</div>
+						</div>
+					</nav>
+				</header>
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container mx-auto flex justify-between">
+					<div>
+						<div className="font-light">epic</div>
+						<div className="font-bold">notes</div>
+					</div>
+					<p>Built with ♥️ by {data.username}</p>
+				</div>
+				<div className="h-5" />
+				<Scripts />
+				<LiveReload />
+			</body>
     </html>
   );
 }
