77ed6a444578496a8e2cf3e5b6626111c6024276
Saurabh Prakash <saurabhp75@gmail.com>
2024-01-08 22:34:45 +0530

    Install @mswjs/data for mock db

--- app/utils/singleton.server.ts
+++ app/utils/singleton.server.ts
@@ -3,6 +3,7 @@
 // Borrowed/modified from https://github.com/jenseng/abuse-the-platform/blob/2993a7e846c95ace693ce61626fa072174c8d9c7/app/utils/singleton.ts
 
 export function singleton<Value>(name: string, value: () => Value): Value {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const yolo = global as any
 	yolo.__singletons ??= {}
 	yolo.__singletons[name] ??= value()
