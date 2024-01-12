923a0f631e95907b4add918a08e4a863de1d4dfe
Saurabh Prakash <saurabhp75@gmail.com>
2024-01-12 12:32:57 +0530

    Use conform form utilities.

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,4 +1,5 @@
-import { parse } from '@conform-to/zod'
+import { conform, useForm } from '@conform-to/react'
+import { getFieldsetConstraint, parse } from '@conform-to/zod'
 import { Label } from '@radix-ui/react-label'
 import {
 	json,
@@ -7,7 +8,6 @@ import {
 	type ActionFunctionArgs,
 } from '@remix-run/node'
 import { Form, useActionData, useLoaderData } from '@remix-run/react'
-import { useEffect, useRef, useState } from 'react'
 import { z } from 'zod'
 import { GeneralErrorBoundary } from '~/components/error-boundary'
 import { floatingToolbarClassName } from '~/components/floating-toolbar'
@@ -16,11 +16,7 @@ import { Input } from '~/components/ui/input'
 import { StatusButton } from '~/components/ui/status-button'
 import { Textarea } from '~/components/ui/textarea'
 import { db } from '~/utils/db.server'
-import {
-	invariantResponse,
-	useFocusInvalid,
-	useIsSubmitting,
-} from '~/utils/misc'
+import { invariantResponse, useIsSubmitting } from '~/utils/misc'
 
 const titleMaxLength = 100
 const contentMaxLength = 10000
@@ -72,100 +68,65 @@ export async function loader({ params }: LoaderFunctionArgs) {
 	})
 }
 
-function useHydrated() {
-	const [hydrated, setHydrated] = useState(false)
-	useEffect(() => setHydrated(true), [])
-	return hydrated
-}
-
 export default function NoteEdit() {
 	const data = useLoaderData<typeof loader>()
 	const actionData = useActionData<typeof action>()
-	const formRef = useRef<HTMLFormElement>(null)
 	// determine whether this form is submitting
 	const isSubmitting = useIsSubmitting()
 
-	const formId = 'note-editor'
-
-	const fieldErrors =
-		actionData?.status === 'error' ? actionData.submission.error : null
-	// we'll use actionData.submission.error['']
-	// (Yeah, it's weird and will change... https://github.com/edmundhung/conform/issues/211)
-	const formErrors =
-		actionData?.status === 'error' ? actionData.submission.error[''] : null
-
-	const isHydrated = useHydrated()
-
-	const formHasErrors = Boolean(formErrors?.length)
-	const formErrorId = formHasErrors ? 'form-error' : undefined
-	const titleHasErrors = Boolean(fieldErrors?.title?.length)
-	const titleErrorId = titleHasErrors ? 'title-error' : undefined
-	const contentHasErrors = Boolean(fieldErrors?.content?.length)
-	const contentErrorId = contentHasErrors ? 'content-error' : undefined
-
-	useFocusInvalid(formRef.current, actionData?.status === 'error')
+	const [form, fields] = useForm({
+		// we can use useId hook to get the id
+		id: 'note-editor',
+		constraint: getFieldsetConstraint(NoteEditorSchema),
+		lastSubmission: actionData?.submission,
+		// Conform enables validation of formData
+		// on the client without n/w request.
+		onValidate({ formData }) {
+			return parse(formData, { schema: NoteEditorSchema })
+		},
+		defaultValue: {
+			title: data.note.title,
+			content: data.note.content,
+		},
+	})
 
 	return (
 		<div className="absolute inset-0">
 			<Form
-				id={formId}
-				// Use noValidate to disable client-side validation
-				// and test out the server-side validation.
-				// Disable HTML validation only when JS has loaded, as we expect JS
-				// todo client side validation, which will be done using ConformJS
-				noValidate={isHydrated}
 				method="POST"
 				className="flex h-full flex-col gap-y-4 overflow-y-auto overflow-x-hidden px-10 pb-28 pt-12"
-				aria-invalid={formHasErrors || undefined}
-				// aria-error is not supported on all browsers
-				// so we use aria-describedby
-				aria-describedby={formErrorId}
-				// Add a tabIndex={-1} here so we can programmatically focus on the form
-				// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/tabindex
-				ref={formRef}
-				tabIndex={-1}
+				{...form.props}
 			>
 				<div className="flex flex-col gap-1">
 					<div>
 						<Label htmlFor="note-title">Title</Label>
-						<Input
-							id="note-title"
-							name="title"
-							defaultValue={data.note.title}
-							required
-							maxLength={titleMaxLength}
-							aria-invalid={titleHasErrors || undefined}
-							aria-describedby={titleErrorId}
-							autoFocus
-						/>
+						<Input autoFocus {...conform.input(fields.title)} />
 						<div className="min-h-[32px] px-4 pb-3 pt-1">
-							<ErrorList id={titleErrorId} errors={fieldErrors?.title} />
+							<ErrorList
+								id={fields.title.errorId}
+								errors={fields.title.errors}
+							/>
 						</div>
 					</div>
 					<div>
-						<Label htmlFor="note-content">Content</Label>
-						<Textarea
-							id="note-content"
-							name="content"
-							defaultValue={data.note.content}
-							required
-							maxLength={contentMaxLength}
-							aria-invalid={contentHasErrors || undefined}
-							aria-describedby={contentErrorId}
-						/>
+						<Label htmlFor={fields.content.id}>Content</Label>
+						<Textarea {...conform.textarea(fields.content)} />
 						<div className="min-h-[32px] px-4 pb-3 pt-1">
-							<ErrorList id={contentErrorId} errors={fieldErrors?.content} />
+							<ErrorList
+								id={fields.content.errorId}
+								errors={fields.content.errors}
+							/>
 						</div>
 					</div>
 				</div>
-				<ErrorList id={formErrorId} errors={formErrors} />
+				<ErrorList id={form.errorId} errors={form.errors} />
 			</Form>
 			<div className={floatingToolbarClassName}>
-				<Button form={formId} variant="destructive" type="reset">
+				<Button form={form.id} variant="destructive" type="reset">
 					Reset
 				</Button>
 				<StatusButton
-					form={formId}
+					form={form.id}
 					type="submit"
 					disabled={isSubmitting}
 					status={isSubmitting ? 'pending' : 'idle'}
