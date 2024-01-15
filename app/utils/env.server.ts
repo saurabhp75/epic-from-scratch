95bdf039452ec1e1cc994c1c336ff460bf6d3d7a
Saurabh Prakash <saurabhp75@gmail.com>
2024-01-16 01:36:38 +0530

    Add honeypot secret for seeding the honeypot inputs

--- app/utils/env.server.ts
+++ app/utils/env.server.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const schema = z.object({
 	NODE_ENV: z.enum(['production', 'development', 'test'] as const),
+	HONEYPOT_SECRET: z.string(),
 })
 
 declare global {
--- app/utils/honeypot.server.ts
+++ app/utils/honeypot.server.ts
@@ -2,4 +2,5 @@ import { Honeypot } from 'remix-utils/honeypot/server'
 
 export const honeypot = new Honeypot({
 	validFromFieldName: process.env.TESTING ? null : undefined,
+	encryptionSeed: process.env.HONEYPOT_SECRET,
 })
