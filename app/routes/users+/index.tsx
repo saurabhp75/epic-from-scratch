8e3b9cfa9527b9e91125fce05c7ee6023c0c64c7
Saurabh Prakash <saurabhp75@gmail.com>
2024-01-21 06:29:03 +0530

    Order the users by note update date

--- app/routes/users+/index.tsx
+++ app/routes/users+/index.tsx
@@ -39,6 +39,13 @@ export async function loader({ request }: DataFunctionArgs) {
 		LEFT JOIN UserImage ON UserImage.userId = User.id
 		WHERE User.username LIKE ${like}
 		OR User.name LIKE ${like}
+		ORDER BY (
+			SELECT Note.updatedAt
+			FROM Note
+			WHERE Note.ownerId = user.id
+			ORDER BY Note.updatedAt DESC
+			LIMIT 1
+		) DESC
 		LIMIT 50
 	`
 
