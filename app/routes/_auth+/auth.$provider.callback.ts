f0c6d71a44312c5b793ab449c1303d43a03c994e
Saurabh Prakash <saurabhp75@gmail.com>
2024-02-04 00:51:34 +0530

    Handle OAuth connection exceptions

--- app/routes/_auth+/auth.$provider.callback.ts
+++ app/routes/_auth+/auth.$provider.callback.ts
@@ -1,6 +1,7 @@
 import { type LoaderFunctionArgs } from '@remix-run/node'
-import { authenticator } from '~/utils/auth.server'
+import { authenticator, getUserId } from '~/utils/auth.server'
 import { ProviderNameSchema, providerLabels } from '~/utils/connections'
+import { prisma } from '~/utils/db.server'
 import { redirectWithToast } from '~/utils/toast.server'
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
@@ -25,7 +26,32 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 	// to the login page with a toast message indicating that there was an error
 	// authenticating with the provider.
 
-	console.log({ profile })
+	// console.log({ profile })
+
+	// check db for an existing connection
+	// via the providerName and providerId (profile.id) and select the userId
+	const existingConnection = await prisma.connection.findUnique({
+		select: { userId: true },
+		where: {
+			providerName_providerId: { providerName, providerId: profile.id },
+		},
+	})
+
+	const userId = await getUserId(request)
+
+	// if there's an existing connection and a userId, then there's a conflict... Either:
+	// 1. The account is already connected to their own account
+	// 2. The account is already connected to someone else's account
+	// redirect to /settings/profile/connections with apprpropriate toast message
+	if (existingConnection && userId) {
+		throw await redirectWithToast('/settings/profile/connections', {
+			title: 'Already Connected',
+			description:
+				existingConnection.userId === userId
+					? `Your "${profile.username}" ${label} account is already connected.`
+					: `The "${profile.username}" ${label} account is already connected to another account.`,
+		})
+	}
 
 	throw await redirectWithToast('/login', {
 		title: 'Auth Success (jk)',
