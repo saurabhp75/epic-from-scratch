6f9de131b8a63772bf6522ad7fa77740db4e68a7
Saurabh Prakash <saurabhp75@gmail.com>
2024-02-13 23:26:52 +0530

    Add a simple unit test using vitest

---
+++ app/utils/misc.error-message.test.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker'
+// you'll get vi from here
+import { expect, test } from 'vitest'
+import { getErrorMessage } from './misc'
+
+test('Error object returns message', () => {
+	const message = faker.lorem.words(2)
+	expect(getErrorMessage(new Error(message))).toBe(message)
+})
+
+test('String returns itself', () => {
+	const message = faker.lorem.words(2)
+	expect(getErrorMessage(message)).toBe(message)
+})
+
+test('undefined falls back to Unknown', () => {
+	// use spyOn on the console's error property
+	// mock the implementation with a function that does nothing (() => {})˘
+	expect(getErrorMessage(undefined)).toBe('Unknown Error')
+	// make sure console.error was once
+	// assert that it was called with the right arguments
+	// restore the mock so we don't swallow errors for other tests.
+})
---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	plugins: [react()],
+	css: { postcss: { plugins: [] } },
+	test: {
+		include: ['./app/**/*.test.{ts,tsx}'],
+		coverage: {
+			include: ['app/**/*.{ts,tsx}'],
+			all: true,
+		},
+	},
+})
