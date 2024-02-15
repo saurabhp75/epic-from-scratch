906a892d30895b3bbfab7ac1580b04e22d5b0ee9
Saurabh Prakash <saurabhp75@gmail.com>
2024-02-15 06:10:05 +0530

    Add authenticated integration test using Remix stub witth multiple routes

--- app/routes/users+/$username.test.tsx
+++ app/routes/users+/$username.test.tsx
@@ -7,6 +7,8 @@ import { createRemixStub } from '@remix-run/testing'
 import { render, screen } from '@testing-library/react'
 import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import { test } from 'vitest'
+import { type loader as rootLoader } from '~/root'
+import { honeypot } from '~/utils/honeypot.server'
 import { default as UsernameRoute, type loader } from './$username'
 
 function createFakeUser() {
@@ -69,4 +71,64 @@ test('The user profile when not logged in as self', async () => {
 	// screen.debug()
 	await screen.findByRole('img', { name: user.name })
 	await screen.findByRole('link', { name: `${user.name}'s notes` })
+})
+
+test('The user profile when logged in as self', async () => {
+	const user = createFakeUser()
+	const App = createRemixStub([
+		// the root route's path should be "/" and it also needs an id of "root"
+		// because our utility for getting the user requires it (check #app/utils/user.ts)
+		// for the loader, you can do the same sort of thing we do for the username
+		// route loader, just import the type of the rootLoader and use that.
+		// the root loader stub will need to return the same properties the real one does
+		// but you can fake them out. So you may want to check out the root loader
+		{
+			id: 'root',
+			path: '/',
+			loader(): Awaited<ReturnType<typeof rootLoader>> {
+				const honeyProps = honeypot.getInputProps()
+				return json({
+					ENV: { MODE: 'test' },
+					theme: 'light',
+					username: 'testuser',
+					toast: null,
+					user: {
+						...user,
+						roles: [],
+					},
+					csrfToken: 'test-csrf-token',
+					honeyProps,
+				})
+			},
+			children: [
+				{
+					// nest this route inside a root route that provides the root loader's data
+					// which you'll find in app/root.tsx
+					path: 'users/:username',
+					Component: UsernameRoute,
+					loader(): Awaited<ReturnType<typeof loader>> {
+						return json({
+							user,
+							userJoinedDisplay: user.createdAt.toLocaleDateString(),
+						})
+					},
+				},
+			],
+		},
+	])
+
+	const routeUrl = `/users/${user.username}`
+	await render(<App initialEntries={[routeUrl]} />, {
+		wrapper: ({ children }) => (
+			<AuthenticityTokenProvider token="test-csrf-token">
+				{children}
+			</AuthenticityTokenProvider>
+		),
+	})
+
+	await screen.findByRole('heading', { level: 1, name: user.name })
+	await screen.findByRole('img', { name: user.name })
+	await screen.findByRole('button', { name: /logout/i })
+	await screen.findByRole('link', { name: /my notes/i })
+	await screen.findByRole('link', { name: /edit profile/i })
 })
